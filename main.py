2027462c71fa1826f7c5f66d81db3f345f553069
Hanaglitched <neincredits@knou.ac.kr>
2022-07-29 14:20:55 +0900

    update for new database

--- main.py
+++ main.py
@@ -25,27 +25,81 @@ cursor = connection.cursor()
 # -----------------------------------------------
 # (2) dataframe introduce area
 # open json file
-with open('D:/my projects/jsonintodb/venv/data/json/output2.json', 'r', encoding='utf-8') as file:
+with open('D:/my projects/jsonintodb/venv/data/json/AllPrices.json', 'r', encoding='utf-8') as file:
     getImport = json.load(file)
 
 # get original dataframe
-df = pd.DataFrame(getImport)
-uuid = df[["key"]]
+df = getImport.get("data")
+uuid = pd.DataFrame(df);
+uuid = uuid.transpose();
 
 # uuid insertion
 
 # Insert DataFrame by once
+
+print(len(uuid.index))
 val = ""
-for j in range(0, len(uuid.index)):
-    val += "(\"" + uuid.iloc[j]['key'] + "\")"
-    if j < len(uuid.index) - 1:
-        val += ","
-sql_ = "INSERT INTO MAGICNOTIFY_UUID_NAME (`key`) VALUES " + val + ";"
-cursor.execute(sql_)
+# for j in range(0, len(uuid.index)):
+#     val = ""
+#     print(j)
+#     try:
+#         val = "(\"" + uuid.index[j] + "\")"
+#         sql_ = "INSERT INTO MagicnotifyUuidName (`key`) VALUES " + val + ";"
+#         cursor.execute(sql_)
+#     except:
+#         pass
+# connection.commit()
 print("uuid insertion done")
 
+uuid = uuid.replace({np.nan: None})
 
+<<<<<<< HEAD
 # push datas into database, by numbers
+print("b")
+for i in range(11, len(uuid.index)):
+    # check the progress; you could make it progress bar to get it visualize.
+    print(i)
+    val = ""
+    key = uuid.index[i]
+    if uuid.iloc[i].get("paper") != None:
+        if uuid.iloc[i].get("paper").get("cardkingdom") != None:
+            if uuid.iloc[i].get("paper").get("cardkingdom").get("retail") != None:
+                tmp = uuid.iloc[i].get("paper").get("cardkingdom").get("retail")
+                length_normal = 0
+                length_foil = 0
+                length = 0
+                normalExistFlag = False
+                foilExistFlag = False
+                if tmp.get("normal") != None:
+                    getNormalDict = tmp.get("normal")
+                    length_normal = len(tmp.get("normal"))
+                    normalExistFlag = True
+                if tmp.get("foil") != None:
+                    getFoilDict = tmp.get("foil")
+                    length_foil = len(tmp.get("foil"))
+                    foilExistFlag = True
+                if length < length_normal:
+                    length = length_normal
+                if length < length_foil:
+                    length = length_foil
+                for j in range(0, length):
+                    if normalExistFlag and j < length_normal:
+                        date = list(getNormalDict)[j]
+                        normal = str(getNormalDict[date])
+                    else:
+                        normal = ""
+                    if foilExistFlag and j < length_foil:
+                        date = list(getFoilDict)[j]
+                        foil = str(getFoilDict[date])
+                    else:
+                        foil = ""
+                    val += "(NULLIF(\"" + foil + "\",\'\') , NULLIF(\"" + normal + "\",\'\') , \"" + date + "\",\"" + key + "\")"
+                    if j < length - 1:
+                        val += ","
+    if val != "":
+        sql = "INSERT INTO MagicnotifyPrice (`foil`, `normal`, `date`, `key`) VALUES " + val + ";"
+=======
+# (3) push datas into database, by numbers
 for i in range(0, len(df.index)):
     # check the progress; you could make it progress bar to get it visualize.
     print(i)
@@ -75,7 +129,7 @@ for i in range(0, len(df.index)):
         # replace NaN with None;
         # http://daplus.net/python-pandas-%EB%98%90%EB%8A%94-numpy-nan%EC%9D%84-none%EC%9C%BC%EB%A1%9C-%EB%8C%80%EC%B2%B4%ED%95%98%EC%97%AC-mysqldb%EC%99%80-%ED%95%A8%EA%BB%98-%EC%82%AC%EC%9A%A9/
         price_moda = price_moda.replace({np.nan: None})
-        # 2. price insertion
+        # price insertion
         for j in range(0, len(price_moda.index)):
             # introducing variables
             foil = str(price_moda.iloc[j]['foil'])
@@ -87,13 +141,14 @@ for i in range(0, len(df.index)):
             if j < len(price_moda.index) - 1:
                 val += ","
         sql = "INSERT INTO MAGICNOTIFY_PRICE (`foil`, `normal`, `date`, `key`) VALUES " + val + ";"
+>>>>>>> b0eb01f1ea2d85b00e80642e55970aade4d6ce0c
         cursor.execute(sql)
 
 connection.commit()
 # -----------------------------------------------
 
 # -----------------------------------------------
-# (4) get elapsed time (for test)
+# (3) get elapsed time (for test)
 end_time = datetime.now()
 elapsed_time = end_time - start_time
 print(elapsed_time)
